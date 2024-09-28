7f55233e9de5b5e8a69e28204efa2f63eaedb61c
FalsePhilosopher <joshgarland90@gmail.com>
2024-09-28 02:07:49 -0600

    Formatting?

--- zst.sh
+++ zst.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /tmp
-gh release download -p '*.zst*' -p 'dictionary' -R FalsePhilosopher/Infosec-Cheatsheets
+gh release download -p 'Infosec-Cheatsheets.tar.zst*' -p 'dictionary' -R FalsePhilosopher/Infosec-Cheatsheets
 cat /tmp/Infosec-Cheatsheets.tar.zst* > /tmp/Infosec-Cheatsheets.tar.zst
+rm Infosec-Cheatsheets.tar.zsta*
 tar --use-compress-program "zstd -d --rm -T0 -D dictionary" -xvf "Infosec-Cheatsheets.tar.zst" --directory $HOME/Downloads
-rm dictionary Infosec-Cheatsheets.tar.zst*
+rm dictionary Infosec-Cheatsheets.tar.zst
