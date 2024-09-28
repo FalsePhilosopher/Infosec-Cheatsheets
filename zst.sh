6370da2d199ae1012fb7655a622d42e92fdcd9ca
FalsePhilosopher <108245223+FalsePhilosopher@users.noreply.github.com>
2024-09-27 23:06:58 -0600

    Update zst.sh
    `--rm` doesn't want to play nice.

--- zst.sh
+++ zst.sh
@@ -1,4 +1,5 @@
 cd /tmp
 gh release download -p '*.zst*' -p 'dictionary' -R FalsePhilosopher/Infosec-Cheatsheets
 cat Infosec-Cheatsheets.tar.zst* > Infosec-Cheatsheets.tar.zst
-tar --use-compress-program "zstd -d --rm -T0 -D dictionary" -xvf "Infosec-Cheatsheets.tar.zst" -c $HOME/Downloads
+tar --use-compress-program "zstd -d -T0 -D dictionary" -xvf "Infosec-Cheatsheets.tar.zst" -c $HOME/Downloads
+rm dictionary Infosec-Cheatsheets.tar.zst*
