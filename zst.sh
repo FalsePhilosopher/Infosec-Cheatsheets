379490793d6e88b0191a16515e78881910e51369
FalsePhilosopher <108245223+FalsePhilosopher@users.noreply.github.com>
2024-09-27 23:53:55 -0600

    Update zst.sh

--- zst.sh
+++ zst.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 cd /tmp
 gh release download -p '*.zst*' -p 'dictionary' -R FalsePhilosopher/Infosec-Cheatsheets
-cat Infosec-Cheatsheets.tar.zst* > Infosec-Cheatsheets.tar.zst
+cat /tmp/Infosec-Cheatsheets.tar.zst* > /tmp/Infosec-Cheatsheets.tar.zst
 tar --use-compress-program "zstd -d --rm -T0 -D dictionary" -xvf "Infosec-Cheatsheets.tar.zst" -c $HOME/Downloads
 rm dictionary Infosec-Cheatsheets.tar.zst*
