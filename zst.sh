aa033f562c0aa304929de334653a1522d210b08e
FalsePhilosopher <108245223+FalsePhilosopher@users.noreply.github.com>
2024-10-03 02:55:02 -0600

    Update zst.sh

--- zst.sh
+++ zst.sh
@@ -5,9 +5,10 @@
 #rm -rf .git
 #b2rsum -o
 #cd ..
-#tar --use-compress-program "zstd -T0 -19" -cvf Infosec-Cheatsheets.tar.zst Infosec-Cheatsheets
-#gpg --sign Infosec-Cheatsheets.tar.zst
-#split -b 1999M Infosec-Cheatsheets.tar.zst Infosec-Cheatsheets.tar.zst && split -b 1999M Infosec-Cheatsheets.tar.zst.gpg Infosec-Cheatsheets.tar.zst.gpg
+#tar --use-compress-program "zstd -T0 -19" -cf - Infosec-Cheatsheets | split --bytes=1999M - Infosec-Cheatsheets.tar.zst 
+##tar --use-compress-program "zstd -T0 -19" -cvf Infosec-Cheatsheets.tar.zst Infosec-Cheatsheets
+##gpg --sign Infosec-Cheatsheets.tar.zst
+##split -b 1999M Infosec-Cheatsheets.tar.zst Infosec-Cheatsheets.tar.zst && split -b 1999M Infosec-Cheatsheets.tar.zst.gpg Infosec-Cheatsheets.tar.zst.gpg
 
 cd /tmp/
 #gh release download -p 'Infosec-Cheatsheets.tar.zst*' -p 'Infosec-Cheatsheets.tar.zst.sig' -R FalsePhilosopher/Infosec-Cheatsheets
@@ -19,4 +20,4 @@ rm Infosec-Cheatsheets.tar.zst*
 #tar --use-compress-program "zstd -d -T0" -xvf "Infosec-Cheatsheets.tar.zst" --directory $HOME/Downloads
 #rm Infosec-Cheatsheets.tar.zst
 cd $HOME/Downloads/Infosec-Cheatsheets/
-b2sum -oRESULTS -c $HOME/Downloads/Infosec-Cheatsheets/BLAKE2SUMS && echo "ALL OK" || echo "WOW! Something fishy's going on"
+b2sum -oRESULTS -c BLAKE2SUMS && echo "ALL OK" || echo "WOW! Something fishy's going on"
